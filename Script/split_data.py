cfba9b549fab9e7cc8ad1ecff4fd98873d5720b5
Gabriele <gabriele.barreca93@hotmail.com>
2019-06-24 17:52:34 +0200

    change

--- Script/split_data.py
+++ Script/split_data.py
@@ -1,11 +1,10 @@
 import sklearn.preprocessing as sk_p
 import pandas as pd
 import numpy as np
-import sys
 
-data_path = sys.argv[1]
+data_path = "../DATA/ML-CUP18-TR.csv"
 
-data = pd.read_csv(data_path, header='infer', engine='python')
+data = pd.read_csv(data_path, header='infer', comment='#')
 
 # scale data
 normalized_data = pd.DataFrame(sk_p.scale(data))
@@ -24,12 +23,12 @@ print(type(drop_ind))
 
 test_set = normalized_data.iloc[drop_ind, :]
 
-filepath = "test_set.csv"
+filepath = "../DATA/test_set.csv"
 file = open(filepath, mode='w')
 test_set.to_csv(file, sep=',', header=False, index=False)
 
 training_set = normalized_data.drop(drop_ind)
 
-filepath = "training_set.csv"
+filepath = "../DATA/training_set.csv"
 file = open(filepath, mode='w')
 training_set.to_csv(file, sep=',', header=False, index=False)
