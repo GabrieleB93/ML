d230400b51c210789bedc75e6ce4806056c4e77b
Gabriele <gabriele.barreca93@hotmail.com>
2019-06-27 18:28:39 +0200

    SVR

--- Script/split_data.py
+++ Script/split_data.py
@@ -1,43 +1,47 @@
-import sklearn.preprocessing as sk_p
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 
-data_path = "../DATA/ML-CUP18-TR.csv"
 
-data = pd.read_csv(data_path, header=None, comment='#')
+def main():
+    data_path = "../DATA/ML-CUP18-TR.csv"
 
-# scale data
+    data = pd.read_csv(data_path, header=None, comment='#')
 
-scaler = StandardScaler()
+    # scale data
+    scaler = StandardScaler()
 
-data.iloc[:, 1:11] = scaler.fit_transform(data.iloc[:, 1:11])
+    data.iloc[:, 1:11] = scaler.fit_transform(data.iloc[:, 1:11])
 
-normalized_data = data
+    normalized_data = data
 
-print(normalized_data)
-print("STANDARD DEVIATION:")
-print(np.std(normalized_data))
-print("MEAN:")
-print(np.mean(normalized_data))
+    print(normalized_data)
+    print("STANDARD DEVIATION:")
+    print(np.std(normalized_data))
+    print("MEAN:")
+    print(np.mean(normalized_data))
 
-remove_n = int(len(data.index)/10)
-print(remove_n)
+    remove_n = int(len(data.index) / 10)
+    print(remove_n)
 
-drop_ind = np.random.choice(normalized_data.index, remove_n, replace=False)
+    drop_ind = np.random.choice(normalized_data.index, remove_n, replace=False)
 
-print(type(drop_ind))
+    print(type(drop_ind))
 
-test_set = normalized_data.iloc[drop_ind, :]
-print(test_set.shape)
+    test_set = normalized_data.iloc[drop_ind, :]
+    print(test_set.shape)
 
-filepath = "../DATA/test_set.csv"
-file = open(filepath, mode='w')
-test_set.to_csv(file, sep=',', header=False, index=False)
+    filepath = "../DATA/test_set.csv"
+    file = open(filepath, mode='w')
+    test_set.to_csv(file, sep=',', header=False, index=False)
 
-training_set = normalized_data.drop(drop_ind)
-print(training_set.shape)
+    training_set = normalized_data.drop(drop_ind)
+    print(training_set.shape)
 
-filepath = "../DATA/training_set.csv"
-file = open(filepath, mode='w')
-training_set.to_csv(file, sep=',', header=False, index=False)
+    filepath = "../DATA/training_set.csv"
+    file = open(filepath, mode='w')
+    training_set.to_csv(file, sep=',', header=False, index=False)
+
+
+if __name__ == '__main__':
+    main()
