3dbf9a69b815feb8c220ca706c91386b850be973
gdrax <gioele.bert@gmail.com>
2019-06-28 14:50:08 +0200

    svr

--- Script/SVM_newGridS.py
+++ Script/SVM_newGridS.py
@@ -20,9 +20,16 @@ def main():
         # plt.title('Heatmap correlation')
         # plt.show()
 
-        print(data_RBF.sort_values('mee'))
+        sorted = data_RBF.sort_values('mee')
+
         best_row_POLY = data_POLY[data_POLY.mee == data_POLY.mee.min()]
         best_row_RBF = data_RBF[data_RBF.mee == data_RBF.mee.min()]
+
+        C_RBF_2 = sorted[sorted['C'] != float(best_row_RBF['C'])].iloc[0]['C']
+        gamma_RBF_2 = sorted[sorted['gamma'] != float(best_row_RBF['gamma'])].iloc[0]['gamma']
+        C_POLY_2 = sorted[sorted['C'] != float(best_row_POLY['C'])].iloc[0]['C']
+
+        print(C_RBF_2)
 
         C_RBF = best_row_RBF.iloc[0]['C']
         C_POLY = best_row_POLY.iloc[0]['C']
