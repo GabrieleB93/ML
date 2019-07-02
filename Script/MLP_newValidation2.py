1ccf27c99184af24ae0e2acbea54497073d42152
Gioele Bertoncini <gioele.bert@gmail.com>
2019-07-02 23:58:55 +0200

    Mlp learning curves

--- Script/MLP_newValidation2.py
+++ Script/MLP_newValidation2.py
@@ -1,10 +1,14 @@
 from utils import *
+from config import *
 
-grid_result_path = "../DATA/grid_search_result_MPL_old.csv"
+grid_result_path = "../DATA/grid_search_result_MLP_OLD"
 
 
 def main():
-    X_train, Y_train, X_val, Y_val = split_development_set(20)
+    # X_train, Y_train, X_val, Y_val = split_development_set(20)
+
+    X_train, Y_train = getTrainData(TRAIN_SET, '1:11', '11:14', ',')
+    X_val, Y_val = getTrainData(VAL_SET, '1:11', '11:14', ',')
 
     if exists(grid_result_path):
         grid_result = pd.read_csv(grid_result_path, sep=',', index_col=False)
@@ -18,22 +22,22 @@ def main():
         momentum = 0.9
 
         train_and_plot_MLP(X_train, Y_train, X_val, Y_val, n_layers, hidden_layers_units, learning_rate, momentum,
-                           batch, epochs)
+                           batch, epochs, 0)
 
         train_and_plot_MLP(X_train, Y_train, X_val, Y_val, n_layers, hidden_layers_units, learning_rate, momentum,
-                           128, epochs)
+                           batch, epochs, 0.001)
 
         train_and_plot_MLP(X_train, Y_train, X_val, Y_val, n_layers, hidden_layers_units, learning_rate, momentum,
-                           915, epochs)
+                           128, 0.0001)
 
         train_and_plot_MLP(X_train, Y_train, X_val, Y_val, n_layers, hidden_layers_units, learning_rate, 0.6,
-                           915, epochs)
-
-        train_and_plot_MLP(X_train, Y_train, X_val, Y_val, n_layers, hidden_layers_units, 0.0004, 0.6,
-                           915, epochs)
-
-        train_and_plot_MLP(X_train, Y_train, X_val, Y_val, n_layers, hidden_layers_units, 0.0004, 0.6,
-                           915, 10000)
+                           915, 0.0002)
+        #
+        # train_and_plot_MLP(X_train, Y_train, X_val, Y_val, n_layers, hidden_layers_units, 0.0004, 0.6,
+        #                    915, epochs)
+        #
+        # train_and_plot_MLP(X_train, Y_train, X_val, Y_val, n_layers, hidden_layers_units, 0.0004, 0.6,
+        #                    915, 10000)
 
 
 if __name__ == '__main__':
