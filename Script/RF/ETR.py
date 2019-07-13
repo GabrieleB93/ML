a50a86218071ddeb07d077f0bfa6a245e75df46d
Gabriele <gabriele.barreca93@hotmail.com>
2019-07-13 14:49:46 +0200

    riorganizzazione

--- Script/RF/ETR.py
+++ Script/RF/ETR.py
@@ -36,7 +36,7 @@ def rfr_model(x, y):  # Perform Grid-Search
 
         grid_result = gsc.fit(x, y)
         best_params = grid_result
-        print_and_saveGrid(grid_result, True, False, 'grid_search_result_ETR', 'ETR')
+        print_and_saveGrid(grid_result, True, False, 'grid_search_result_ETR_NEW', 'ETR')
         # print(best_params)
         # print(grid_result.cv_results_['mean_test_score'])
         print(grid_result.cv_results_['mean_test_mee'])
@@ -53,7 +53,7 @@ def rfr_model(x, y):  # Perform Grid-Search
         n_estimators = int(best_row.iloc[0]['n_estimators'])
         bootstrap = best_row.iloc[0]['bootstrap']
         max_features = int(best_row.iloc[0]['max_features'])
-        # min_samples_split = int(best_row.iloc[0]['min_samples_split'])
+        min_samples_split = int(best_row.iloc[0]['min_samples_split'])
 
         if bootstrap:
             result = True
@@ -61,22 +61,23 @@ def rfr_model(x, y):  # Perform Grid-Search
             result = False
 
         rfr = ExtraTreesRegressor(max_depth=max_depth, n_estimators=n_estimators,
-                                  # min_samples_split=min_samples_split,
+                                  min_samples_split=min_samples_split,
                                   max_features=max_features,
                                   bootstrap=bootstrap,
+                                  max_leaf_nodes=None,
                                   random_state=False, verbose=False, oob_score=result)
 
         print(best_row)
         if CV:
             print('IN CV')
-            plt.figure()
-            plt.title('LEARNING CURVE')
+            fig = plt.figure()
+            plt.title('LEARNING CURVE ETR')
             plt.xlabel("Training examples")
-            plt.ylabel("Score")
+            plt.ylabel("MSE")
             train_sizes, train_scores, test_scores = learning_curve(
                 rfr, x, y, cv=3, n_jobs=-1
                 # , scoring=make_scorer(mean_euclidean_error)
-                # , scoring='neg_mean_squared_error'
+                , scoring='neg_mean_squared_error'
             )
 
             print("TRAIN")
@@ -87,10 +88,10 @@ def rfr_model(x, y):  # Perform Grid-Search
             print(train_sizes)
 
             if Plot == 'OLD':
-                train_scores_mean = np.mean(train_scores, axis=1)
-                train_scores_std = np.std(train_scores, axis=1)
-                test_scores_mean = np.mean(test_scores, axis=1)
-                test_scores_std = np.std(test_scores, axis=1)
+                train_scores_mean = abs(np.mean(train_scores, axis=1))
+                train_scores_std = abs(np.std(train_scores, axis=1))
+                test_scores_mean = abs(np.mean(test_scores, axis=1))
+                test_scores_std = abs(np.std(test_scores, axis=1))
                 plt.grid()
 
                 plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
@@ -98,17 +99,30 @@ def rfr_model(x, y):  # Perform Grid-Search
                                  color="r")
                 plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
                                  test_scores_mean + test_scores_std, alpha=0.1, color="g")
-                plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-                         label="Training score")
-                plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-                         label="Cross-validation score")
+                plt.plot(train_sizes, train_scores_mean, '-', color="blue",
+                         label="TR LOSS")
+                plt.plot(train_sizes, test_scores_mean, '--', color="orange",
+                         label="VL LOSS")
 
                 plt.legend(loc="best")
                 plt.show()
+
+                directory = "../../Image/ETR/"
+                t = strftime("%H_%M")
+                file = "ETR_LC" + t + ".png"
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                fig.savefig(directory + file)
+
             elif Plot == 'NEW':
+                train_scores = abs(np.mean(train_scores, axis=1))
+                test_scores = abs(np.mean(test_scores, axis=1))
                 plt.plot(train_scores, label="TR Loss")
                 plt.plot(test_scores, label="VL Loss")
                 plt.show()
+
+
+
             # scores = cross_validate(rfr, x, y, cv=3, scoring=scoring)
             # print(scores)
 
@@ -147,5 +161,8 @@ if __name__ == '__main__':
         parseArg(arg)
         print(arg)
         print(CV)
-        X, Y = getTrainData(CUP, '1:11', '11:13', ',')
+        X, Y = getTrainData(CUP1_NEW2, '1:11', '11:13', ',')
+        scaler = StandardScaler()
+        scaler.fit(X)
+        X = scaler.transform(X)
         rfr_model(X, Y)
--- Script/RF/RFR.py
+++ Script/RF/RFR.py
@@ -51,7 +51,7 @@ def rfr_model(x, y):  # Perform Grid-Search
         n_estimators = int(best_row.iloc[0]['n_estimators'])
         bootstrap = best_row.iloc[0]['bootstrap']
         max_features = int(best_row.iloc[0]['max_features'])
-        # min_samples_split = int(best_row.iloc[0]['min_samples_split'])
+        min_samples_split = int(best_row.iloc[0]['min_samples_split'])
 
         if bootstrap:
             result = True
@@ -59,18 +59,19 @@ def rfr_model(x, y):  # Perform Grid-Search
             result = False
 
         rfr = RandomForestRegressor(max_depth=max_depth, n_estimators=n_estimators,
-                                    # min_samples_split=min_samples_split,
+                                    min_samples_split=min_samples_split,
                                     max_features=max_features,
                                     bootstrap=bootstrap,
+                                    max_leaf_nodes=None,
                                     random_state=False, verbose=False, oob_score=result)
 
         print(best_row)
         if CV:
             print('IN CV')
-            plt.figure()
-            plt.title('LEARNING CURVE')
+            fig = plt.figure()
+            plt.title('LEARNING CURVE RFR')
             plt.xlabel("Training examples")
-            plt.ylabel("Score")
+            plt.ylabel("MSE")
             train_sizes, train_scores, test_scores = learning_curve(
                 rfr, x, y, cv=3, n_jobs=-1
                 # , scoring=make_scorer(mean_euclidean_error)
@@ -85,10 +86,10 @@ def rfr_model(x, y):  # Perform Grid-Search
             print(train_sizes)
 
             if Plot == 'OLD':
-                train_scores_mean = np.mean(train_scores, axis=1)
-                train_scores_std = np.std(train_scores, axis=1)
-                test_scores_mean = np.mean(test_scores, axis=1)
-                test_scores_std = np.std(test_scores, axis=1)
+                train_scores_mean = abs(np.mean(train_scores, axis=1))
+                train_scores_std = abs(np.std(train_scores, axis=1))
+                test_scores_mean = abs(np.mean(test_scores, axis=1))
+                test_scores_std = abs(np.std(test_scores, axis=1))
                 plt.grid()
 
                 plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
@@ -96,13 +97,21 @@ def rfr_model(x, y):  # Perform Grid-Search
                                  color="r")
                 plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
                                  test_scores_mean + test_scores_std, alpha=0.1, color="g")
-                plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
-                         label="Training score")
-                plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
-                         label="Cross-validation score")
+                plt.plot(train_sizes, train_scores_mean, '-', color="blue",
+                         label="TR LOSS")
+                plt.plot(train_sizes, test_scores_mean, '--', color="orange",
+                         label="VL LOSS")
 
                 plt.legend(loc="best")
                 plt.show()
+
+                directory = "../../Image/RFR/"
+                t = strftime("%H_%M")
+                file = "RFR_LC"+t+".png"
+                if not os.path.exists(directory):
+                    os.makedirs(directory)
+                fig.savefig(directory + file)
+
             elif Plot == 'NEW':
                 plt.plot(train_scores, label="TR Loss")
                 plt.plot(test_scores, label="VL Loss")
@@ -146,5 +155,8 @@ if __name__ == '__main__':
         print(arg)
         print(CV)
 
-        X, Y = getTrainData(CUP, '1:11', '11:13', ',')
+        X, Y = getTrainData(CUP_NEW, '1:11', '11:13', ',')
+        scaler = StandardScaler()
+        scaler.fit(X)
+        X = scaler.transform(X)
         rfr_model(X, Y)
