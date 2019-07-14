8927386ffd97c38b7b93200d55d7c61c9df1f134
GarieleB93 <onion_sama@hotmail.it>
2019-07-14 22:57:04 +0200

    final commit

--- Script/split_new.py
+++ Script/split_new.py
@@ -4,12 +4,15 @@ import sys
 from utils import split_development_set
 
 
+# According to the arguments, it will have different effects. See more on REadMe.dm
 def main():
-    if len(sys.argv) == 1:
 
+    # If there is no argument, the script splits the ML2018_TR.csv in training set and test set.
+    if len(sys.argv) == 1:
         data_path = ALL_DATA
         data = pd.read_csv(data_path, header=None, comment='#')
 
+        # Choosing and removing the 10% of row from the original file
         remove_n = int(len(data.index) / 10)
         print(remove_n)
 
@@ -20,6 +23,7 @@ def main():
         test_set = data.iloc[drop_ind, :]
         print(test_set.shape)
 
+        # Save the new files
         file = open(CUP, mode='w')
         test_set.to_csv(file, sep=',', header=False, index=False)
 
@@ -29,8 +33,10 @@ def main():
         file = open(TEST_SET, mode='w')
         training_set.to_csv(file, sep=',', header=False, index=False)
 
+    # If the argument is 'new', then the script will take the Training set (already existing) and draws 2 new sets
     if len(sys.argv) > 1 and sys.argv[1].lower() == 'new':
         X_train, Y_train, X_val, Y_val = split_development_set(20)
+
 
 if __name__ == '__main__':
     main()
