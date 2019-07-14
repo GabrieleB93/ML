2bbb4b97c09949d6bba19572f5638c337ea56602
GarieleB93 <onion_sama@hotmail.it>
2019-07-14 22:52:45 +0200

    final commit

--- Script/Test.py
+++ Script/Test.py
@@ -6,6 +6,7 @@ from utils import *
 from sklearn.svm import SVR
 
 
+# Define the model of a MLP with the best parameters
 def MLP(xtr, ytr, xts, yts):
     start = time()
     model = create_model(learn_rate=0.0001, units=500, level=5, momentum=0.9, lamda=0, activation='relu')
@@ -16,6 +17,7 @@ def MLP(xtr, ytr, xts, yts):
     return mean_euclidean_error(prd, yts), tmp
 
 
+# Define the model of a SVR_RBF with the best parameters
 def SVM(xtr, ytr, xts, yts):
     start = time()
     SVR_RBF = MultiOutputRegressor(SVR(verbose=0, kernel='rbf', C=23.5, epsilon=0.01, gamma=0.1))
@@ -26,6 +28,7 @@ def SVM(xtr, ytr, xts, yts):
     return mean_euclidean_error(prd, yts), tmp
 
 
+# Define the model of a ETR with the best parameters
 def ETR(xtr, ytr, xts, yts=None):
     start = time()
     etr = ExtraTreesRegressor(max_depth=10, n_estimators=500,
@@ -43,20 +46,23 @@ def ETR(xtr, ytr, xts, yts=None):
         return mean_euclidean_error(prd, yts), etr, tmp
 
 
+# According to the arguments, it will have different effects. See more on REadMe.dm
 def main():
-    X_train, Y_train = getTrainData(CUP1, '1:11', '11:13', ',')
-    X_test, Y_test = getTrainData(CUP1, '1:11', '11:13', ',')
-
-    scaler = StandardScaler()
-    scaler.fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-
+    # If there is no arguments, the script test all the models
     if len(sys.argv) == 1:
+        X_train, Y_train = getTrainData(CUP1, '1:11', '11:13', ',')
+        X_test, Y_test = getTrainData(CUP1, '1:11', '11:13', ',')
+
+        scaler = StandardScaler()
+        scaler.fit(X_train)
+        X_train = scaler.transform(X_train)
+        X_test = scaler.transform(X_test)
+
         mlp, mlp_time = MLP(X_train, Y_train, X_test, Y_test)
         svr, svr_time = SVM(X_train, Y_train, X_test, Y_test)
         etr_err, etr_model, etr_time = ETR(X_train, Y_train, X_test, Y_test)
 
+        # Print times and MEE erro for each model
         print("MEE Error MLP: %.3f" % mlp)
         print("Time MLP: %.3f" % mlp_time)
         print("MEE Error SVM: %.3f" % svr)
@@ -64,6 +70,7 @@ def main():
         print("MEE Error ETR: %.3f" % etr_err)
         print("Time ETR: %.3f" % etr_time)
 
+    # If the argument is 'predict', the best model predicts the results for the CUP
     if len(sys.argv) > 1 and sys.argv[1].lower() == 'predict':
         X_All, Y_All = getTrainData(ALL_DATA, '1:11', '11:13', ',')
         X_blind = getBlindData()
@@ -76,10 +83,6 @@ def main():
         etr_model = ETR(X_All, Y_All, X_blind)
         predicted = etr_model.predict(X_blind)
         saveResultsOnCSV(predicted, 'BarBer_ML-CUP18-TS')
-
-        # fig = plt.figure()
-        # plt.plot(predicted[:, 0], predicted[:, 1], 'ro')
-        # plt.show()
 
 
 if __name__ == '__main__':
